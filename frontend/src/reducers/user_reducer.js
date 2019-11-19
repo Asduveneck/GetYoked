afd51cf7b1da9d3dd019e32723338ba4041ffd68
Braeden Austgen <baustgen2615@gmail.com>
2019-11-19 13:57:01 -0800

    import action type

--- frontend/src/reducers/user_reducer.js
+++ frontend/src/reducers/user_reducer.js
@@ -1,4 +1,5 @@
 import { RECEIVE_CURRENT_USER } from "../actions/session_actions";
+import { RECEIVE_USER } from "../actions/user_actions"
 
 const usersReducer = (state = {}, action) => {
   Object.freeze(state);
