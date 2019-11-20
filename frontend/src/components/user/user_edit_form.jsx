191d3f81d56c167d37996541359e2287b12efb40
Braeden Austgen <baustgen2615@gmail.com>
2019-11-19 16:59:11 -0800

    add user components

---
+++ frontend/src/components/user/user_edit_form.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class UserEditForm extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>BUILD THIS (UserEditForm)</h1>
+                <form>
+                    
+                </form>
+            </div>
+        )
+    }
+}
+
+export default UserEditForm;
