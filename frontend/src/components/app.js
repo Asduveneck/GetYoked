26a2ba0211dc88175aac2e7543effeb8e009217f
Asduveneck <asduveneck@ucdavis.edu>
2019-11-21 02:29:03 -0800

    Outlines splash skeleton with crap CSS

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -6,6 +6,7 @@ import LogInFormContainer from './session/login_form_container';
 import SignUpFormContainer from './session/signup_form_container';
 import WorkoutFormContainer from './workouts/workout_form/workout_form_container';
 import TeamMembers from './teampage/team_page'
+import Splash from './splash/splash_page'
 
 import './reset.scss'
 import ProfileContainer from "./user/profile_container";
@@ -22,6 +23,8 @@ const App = () => (
       <AuthRoute path="/signup" component={SignUpFormContainer} />
       {/* routes for workouts */}
       <Route exact path = "/developers" component={TeamMembers} />
+      <Route exact path = "/splash" component={Splash} />
+
     </Switch>
     {/* footer? */}
   </div>
---
+++ frontend/src/components/splash/splash_page.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+import "./splash_page.scss";
+class SplashPage extends React.Component {
+
+  render(){
+    return (
+      <div className="splash_page">
+
+        <div className="what_we_do">
+          <h1>Join the fastest growing techy fitness community</h1>
+          <p>Feel motivated as you unlock rewards and achievements</p>
+        </div>
+
+        <div className="how_you_grow">
+          <h1>How can you grow with our app?</h1>
+
+          <div className="sp_hyg_3_cols">
+            <div className="sp_hyg_col"> {/* column 1*/}
+              {/* corresponding image to */}
+              <h1>Increase your Strength and Durability</h1>
+            </div>
+            <div className="sp_hyg_col"> {/* column 2*/}
+              {/* corresponding image to */}
+              <h1>Enhance your Cardiovascular Health</h1>
+            </div>
+            <div className="sp_hyg_col"> {/* column 3*/}
+              {/* corresponding image to */}
+              <h1>Become more flexible</h1>
+            </div>
+          </div>
+
+        </div>
+
+        <div className="what_we_offer"> 
+            <h2>Lots of pics</h2>
+            {/* Fake screenshots of our app with made up features */}
+        </div>
+
+        <div className="join_us"> 
+        <h2>Sign up link disguised as button</h2>
+        </div>
+        
+      </div> // End of splashPage
+    );
+  }
+}
+
+export default SplashPage;
