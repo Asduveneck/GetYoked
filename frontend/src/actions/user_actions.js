dd0dd9de7d5bf25256d2f0d013f2fc0ef2b7671b
Braeden Austgen <baustgen2615@gmail.com>
2019-11-20 16:16:10 -0800

    Add backend for user update

--- frontend/src/actions/user_actions.js
+++ frontend/src/actions/user_actions.js
@@ -1,4 +1,4 @@
-import { getUser } from '../util/user_api_util';
+import { getUser, updateUser } from '../util/user_api_util';
 
 export const RECEIVE_USER = "RECEIVE_USER";
 
@@ -9,6 +9,12 @@ export const receiveUser = user => ({
 
 export const fetchUser = id => dispatch => (
     getUser(id)
+        .then(user => dispatch(receiveUser(user)))
+        .catch(err => console.log(err))
+);
+
+export const updateUser = user => dispatch => (
+    updateUser(user)
         .then(user => dispatch(receiveUser(user)))
         .catch(err => console.log(err))
 );
