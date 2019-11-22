ac6ec291ecfb919f6ad28757ea62408765bdb170
your name here <maureenmdurnin@gmail.com>
2019-11-21 21:45:34 -0800

    workout page styling

--- frontend/src/components/workouts/workout_description/workout_detail.js
+++ frontend/src/components/workouts/workout_description/workout_detail.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './workout-detail.scss';
+import { Link } from 'react-router-dom';
 
 class WorkoutDetail extends React.Component {
     constructor(props) {
@@ -7,11 +8,30 @@ class WorkoutDetail extends React.Component {
     }
 
     render() {
-        return(
-            <div>
-                this is the workout detail page
+        return (
+          <div className="workout-detail-window">
+
+
+            <div className="workout-detail-header">
+              <div className="workout-detail-pic-cartoon">
+                <img src="/images/exercise_cartoons.jpg" />
+              </div>
             </div>
-        )
+
+            <div className="workout-detail-scroll">
+                <div className="screen">Your workout today</div>
+                <Link to="/workoutnew"><button>Select a different type of workout</button></Link>
+                <div className="workout-detail-description-parent">
+                    <div className="wo-detail-name">Workout Name Placeholder</div>
+                    <div className="wo-detail-item">
+                        <div className="workout-detail-item-pic">Workout picture placeholder</div>
+                        <div className="workout-detail-item-exercise">Exercise description placeholder</div>
+                    </div>
+                </div>
+            </div>
+
+          </div>
+        );
     }
 }
 
