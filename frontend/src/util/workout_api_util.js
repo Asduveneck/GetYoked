0ee2501ba990f99254561e783ac204c76ff569a6
your name here <maureenmdurnin@gmail.com>
2019-11-22 13:09:54 -0800

    fix frontend route for feching workout

--- frontend/src/util/workout_api_util.js
+++ frontend/src/util/workout_api_util.js
@@ -1,9 +1,11 @@
 import axios from "axios";
+import { fileURLToPath } from "url";
+import { DH_UNABLE_TO_CHECK_GENERATOR } from "constants";
 
 export const fetchWorkouts = () => {
   return axios.get("/api/workouts")
 };
 
 export const fetchWorkout = (type, intensity) => {
-  return axios.get(`/api/workouts/find?type=${type}&intensity=${intensity}`);
+  return axios.get(`/api/workouts/workout/find?type=${type}&intensity=${intensity}`);
 };
