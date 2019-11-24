8757a109a0d01b0211d3fe1d32a4e251528dd57d
Zaid Pasha <zaidzpasha@gmail.com>
2019-11-23 20:26:05 -0800

    Semi functional navbar

--- frontend/src/components/greeting/greeting.jsx
+++ frontend/src/components/greeting/greeting.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import "../../stylesheets/components/navbar/mainnav.css";
+
 
 
 class Greeting extends React.Component {
@@ -13,8 +15,25 @@ class Greeting extends React.Component {
       <div>
           <Link className="" to={"/login"}>Sign In</Link>
       </div>
-      )
+      )  
     }; 
+    if (this.props.location.pathname === '/workoutnew') {
+      return (
+        <div>
+          <div className="navRight">
+            <div className="dropdown" >
+              <button className="dropbtn"><img src="ballon.png" height="60px" alt="" /></button>
+              <div className="dropdown-content">
+                <a href="#">Manage Profiles</a>
+                <a href="/#/login" onClick={this.props.logout} >Sign Out</a>
+              </div>
+            </div>
+          </div>
+  
+       
+        </div>
+      )
+    };
   return (null)
   };
 }
