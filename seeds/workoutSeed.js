b81dc0d9cb4ec518a6deae8db52e3a749cc486c1
Asduveneck <asduveneck@ucdavis.edu>
2019-11-24 12:34:26 -0800

    Starting over to debug

--- seeds/workoutSeed.js
+++ seeds/workoutSeed.js
@@ -1,132 +1,28 @@
-const mongoose = require("mongoose");
-const axios = require("axios");
-// const { port, db, secret } = require("../config/env");
-// mongoose.Promise = require("bluebird"); // Not using...
-// mongoose.connect(db);
 
-const Workout = require("../models/Workout");
-const Exercise = require("../models/Exercise");
-
-// Workout.collection.drop(); // FINDME Can't include this until we seed the data first again...
-
-// Helper to return a promise to find a specific workout POJO
-async function exerciseFindOneByBName(name) {
-  console.log("Within exerciseFindOneByName");
-  let exercise = await Exercise.findOne( { name }, function(err, data) {
-    // error handling
-    if (err) {
-      return;
-    }
-    if (data === null || data.length == 0) {
-      console.log("Nothing passed in here?");
-      return;
-    }
-    // return an array of workouts
-    return data;
-  })
-  console.log(`Found:\n${exercise}`);
-  return exercise;
-}; // Used in exerciseArrayResMaker
-
-// Makes array of matched exercises that queries each entry
-async function exerciseArrayResMaker(exercisesArray) {
-  console.log("Hitting exerciseArrayResMaker with:");
-  console.log(exercisesArr);
-  console.log("\n");
-  if (!Array.isArray(workoutsArray)) {
-    return; //stops execution early
-  }
-  let finalExerciseArr = [];
-
-  for(let i = 0; i < exercisesArray.length; i++ ) {
-    console.log(`Within exerciseArrayResMaker. Loop: ${i}\n`);
-    let exercise = await exerciseFindOneByBName(exercisesArray[i]);
-    console.log(`Found Exercise:\n${exercise}`);
-    if(exercise) { finalExerciseArr.push(exercise[0]) };
-  }
-  console.log(`Outside for loop, array to push is: `);
-  console.log(finalExerciseArr);
-  return finalExerciseArr;
-}; // Used in workoutResHandler (final step to seed individual)
-
-// Helper to make workouts based 
-const workoutCreate = (name, type, intensity, description, exercisesArr = []) => {
-  console.log("Within workoutCreate");
-  if (exercisesArr.length === 0) { // If there are no exercises, we should throw an error or warning?
-    console.log("No workouts were found");
-    Workout.create( { name, type, intensity, description } ); 
-  } else { // create a new workout
-    const newWorkout = new Workout({name, type, intensity, description});
-    // Use mongoose push to add a new exercise per workout
-    for (let i = 0; i < exercisesArr.length; i++) {
-      // Maybe add a safe option here, so if the object is null, call next?
-      console.log(`New exercise to add:`);
-      console.log(`\n`);
-      console.log(`${"#".repeat(90)}`);
-      console.log(`\n`);
-      newWorkout.exercises.push(exercisesArr[i]);
-    }
-    // Save to mongoose DB:
-    newWorkout.save();
-  }
-}; 
-
-// utilize all above helper methods // CHECK LINE 65 and BELOW...
-async function workoutResHandler(name, type, intensity, description, exercisesArr = []) {
-  console.log("Beginning of workoutResHandler with exercises:");
-  console.log(exercisesArr);
-  console.log('\n');
-
-  let screenedExercises = [];
-  // if we have something in our exercisesArray
-  if(exercisesArr.length !== 0) {
-    console.log("We have something")
-    // If I pass in a string, wrap it in an array
-    if(typeof(exercisesArr) === "string" ) {
-      screenedExercises.push(exercisesArr)
-    } else if(Array.isArray(exercisesArr)) {
-      screenedExercises = exercisesArr;
-    }
-  };
-  console.log("Will pass the following into exerciseArrayResMaker: ")
-  console.log(screenedExercises);
-  console.log("\n");
-  await exerciseArrayResMaker(screenedExercises) //FINDME BROKEN HERE ALREADY // IT IS THIS .THEN THAT IS BROKEN.
-    .then(exercises => {
-      console.log("Executed exerciseArrayResMaker, and we returned:");
-      console.log(exercises);
-      console.log('\n');
-      workoutCreate(name, type, intensity, description, exercises) // NEVER CALLED?!?
-    })
-    .catch(err => {
-      console.log("Something went wrong in Workout Seeds");
-      console.log(err.messages);
-    })
-}
 
 // Testing
-workoutResHandler(
-"Introduction to Cardio",
- "cardio",
- 1,
- "An introduction to cardio",
- ["Stretch!", "Walk-outs", "Arm circles"]
-);
-workoutResHandler(
-"Introduction to Strength",
- "strength",
- 1,
- "An introduction to strength"//,
-//  ["Stretch!", "Lunge twist", "Squats"]
-);
+// workoutResHandler(
+// "Introduction to Cardio",
+//  "cardio",
+//  1,
+//  "An introduction to cardio",
+//  ["Stretch!", "Walk-outs", "Arm circles"]
+// );
+// workoutResHandler(
+// "Introduction to Strength",
+//  "strength",
+//  1,
+//  "An introduction to strength"//,
+// //  ["Stretch!", "Lunge twist", "Squats"]
+// );
 
-workoutResHandler(
-"Introduction to Flexibility",
- "flexibility",
- 1,
- "An introduction to flexibility"//,
-//  ["Stretch!", "Hip circles", "Scorpion"]
-);
+// workoutResHandler(
+// "Introduction to Flexibility",
+//  "flexibility",
+//  1,
+//  "An introduction to flexibility"//,
+// //  ["Stretch!", "Hip circles", "Scorpion"]
+// );
 
 // Workout.insertMany([
 //   {
@@ -218,35 +114,3 @@ workoutResHandler(
 //       stretch2, stretch2
 //     ]
 //   });
-
-  // ORIGINALLY IN app.js backend
-
-  // let test = require("./models/Workout").collection
-// let test2 = require("./models/Workout"); 
-
-// console.log(
-//   test.findOne({ name: "Introduction to Strength" }, function(err, workout) {
-//     if (err) handleError(err); // NO WORKY
-//     // return workout.toObject(); // NO WORKY 
-//   })
-// );
-
-// console.log("FINDME");
-// console.log(
-//   test2.findOne().lean().exec(function(err, obj) {
-//       obj
-//   })
-// );
-// console.log("FINDME");
-
-
-
-
-
-// test.findOne({ name: "Introduction to Strength" }, function(err, workout) {
-//     if (err) handleError(err); // NO WORKY
-//     console.log(workout); // NO WORKY?
-// }); // WORKY
-
-
-
