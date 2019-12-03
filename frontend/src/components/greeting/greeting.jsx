69895888a9ebd6efe468887b89d9fa8731acbdb0
Braeden Austgen <baustgen2615@gmail.com>
2019-12-03 14:14:06 -0800

    refactor greeting component to be DRY

--- frontend/src/components/greeting/greeting.jsx
+++ frontend/src/components/greeting/greeting.jsx
@@ -17,38 +17,37 @@ class Greeting extends React.Component {
 
   render() {
     debugger;
+    let links;
+
     if (!this.props.user) {
-      return (
-        <div>
-          <div className="navRight">
-            <div className="dropdown" >
-              <button className="dropbtn"><img src="ballon.png" height="60px" alt="" /></button>
-              <div className="dropdown-content">
-                
-                <Link to={"/login"}>Sign In</Link>
-                <Link to={"/signup"}>Sign Up</Link>
-              </div>
-            </div>
-          </div>
+      links = (
+        <div className="dropdown-content">
+          <Link to={"/login"}>Sign In</Link>
+          <Link to={"/signup"}>Sign Up</Link>
+          
         </div>
       )
     } else {
-      return (
-        <div>
-          <div className="navRight">
-            <div className="dropdown" >
-              <button className="dropbtn"><img src="ballon.png" height="60px" alt="" /></button>
-              <div className="dropdown-content">
-                <a href="/#/login" onClick={this.props.logout} >Sign Out</a>
-                <Link to={'/users/' + this.props.user.id}>Profile</Link>
-              </div>
-            </div>
-          </div>
-  
-       
+      links = (
+        <div className="dropdown-content">
+          <a href="/#/login" onClick={this.props.logout} >Sign Out</a>
+          <Link to={'/users/' + this.props.user.id}>Profile</Link>
+          <Link to={'/developers'}>Developers</Link>
         </div>
       )
     };
+    return (
+      <div>
+        <div className="navRight">
+          <div className="dropdown" >
+            <button className="dropbtn"><img src="ballon.png" height="60px" alt="" /></button>
+            {links}
+          </div>
+        </div>
+
+
+      </div>
+    )
   };
 }
 
