69dddb210783823e0c6e13fd07b181927e018d65
Asduveneck <asduveneck@ucdavis.edu>
2020-01-02 11:05:52 -0800

    Reuse classnames to standardize styling

--- frontend/src/components/user/user_edit_form.jsx
+++ frontend/src/components/user/user_edit_form.jsx
@@ -31,49 +31,66 @@ class UserEditForm extends React.Component {
 
     render() {
         return (
-            <div>
-                <h1>Edit User Info</h1>
-                <form onSubmit={this.handleSubmit}>
-                    <label>Username
-                        <p>{this.state.username}</p>
-                    </label>
-                    <label>Age
-                        <input type="text" 
-                            value={this.state.age}
-                            onChange={this.handleChange('age')}
-                        />
-                    </label>
-                    <label>Height
-                        <input type="text" 
-                            value={this.state.height}
-                            onChange={this.handleChange('height')}
-                        />
-                    </label>
-                    <label>Weight
-                        <input type="text" 
-                            value={this.state.weight}
-                            onChange={this.handleChange('weight')}
-                        />
-                    </label>
-                    <label>Activity Level
-                        <select value={this.state.activity} onChange={this.handleChange('activity')}>
-                            <option value="light">Light</option>
-                            <option value="medium">Medium</option>
-                            <option value="hard">Hard</option>
-                        </select>
-                    </label>
-                    <label>Goals
-                        <select value={this.state.goals} onChange={this.handleChange('goals')}>
-                            <option value="5k">5k</option>
-                            <option value="lose weight">Lose Weight</option>
-                            <option value="Michelle Obama arms">Michelle Obama Arms</option>
-                        </select>
-                    </label>
+          <div className="user-info-parent">
+            <h2>Edit User Info</h2>
+            <form onSubmit={this.handleSubmit}>
+              <label>
+                Username
+                <p>{this.state.username}</p>
+              </label>
+              <label>
+                Age
+                <input
+                  type="text"
+                  value={this.state.age}
+                  onChange={this.handleChange("age")}
+                />
+              </label>
+              <label>
+                Height
+                <input
+                  type="text"
+                  value={this.state.height}
+                  onChange={this.handleChange("height")}
+                />
+              </label>
+              <label>
+                Weight
+                <input
+                  type="text"
+                  value={this.state.weight}
+                  onChange={this.handleChange("weight")}
+                />
+              </label>
+              <label>
+                Activity Level
+                <select
+                  value={this.state.activity}
+                  onChange={this.handleChange("activity")}
+                >
+                  <option value="light">Light</option>
+                  <option value="medium">Medium</option>
+                  <option value="hard">Hard</option>
+                </select>
+              </label>
+              <label>
+                Goals
+                <select
+                  value={this.state.goals}
+                  onChange={this.handleChange("goals")}
+                >
+                  <option value="5k">5k</option>
+                  <option value="lose weight">Lose Weight</option>
+                  <option value="Michelle Obama arms">
+                    Michelle Obama Arms
+                  </option>
+                </select>
+              </label>
 
-                    <input type="submit" value="Update Info"/>
-                </form>
-            </div>
-        )
+              <input type="submit" value="Update Info" />
+            </form>
+          </div>
+        );
     }
 }
 
