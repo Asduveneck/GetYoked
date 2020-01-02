7497e6edd41ff00d8c393eb49c20b14b6dcfefbb
Asduveneck <asduveneck@ucdavis.edu>
2020-01-02 09:55:16 -0800

    identified broken axios get request

--- frontend/src/util/user_api_util.js
+++ frontend/src/util/user_api_util.js
@@ -1,11 +1,16 @@
 import axios from 'axios';
 
 export const getUser = id => {
+    console.log(id);
     return axios.get(`/api/users/${id}`)
 };
 
 export const updateUser = user => {
-    return axios.patch(`/api/users/${user.id}`, user)
+    console.log("In user_api_util.\nuser:");
+    console.log(user);
+    console.log(user.id);
+    return axios.get(`/api/users/${user.id}`); // TODO: Found the problem. This user is not the same...?
+    // return axios.patch(`/api/users/${user.id}`, user)
 };
 
 export const updateUserWorkouts = (userId, workout) => {
